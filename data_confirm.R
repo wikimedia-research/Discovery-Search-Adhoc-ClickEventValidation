9e49029ef2017a32a05aa9aab9f19329c0e75195
Mikhail Popov <mpopov@wikimedia.org>
2016-04-28 17:41:25 -0700

    Saving progress (additional investigations)

--- data_confirm.R
+++ data_confirm.R
@@ -1,21 +1,19 @@
-# SELECT * FROM TestSearchSatisfaction2_15357244 WHERE event_action = 'click' LIMIT 10;
-
 end_date <- Sys.Date()-1
-start_date <- end_date - 30
+start_date <- as.Date("2016-04-25")
 # This test does not effect autocomplete (prefix or comp suggest)
 #   and does not have any effect on zero results rate.
 cat("Fetching EL data from", as.character(start_date), "to", as.character(end_date), "...\n")
 events <- do.call(rbind, lapply(seq(start_date, end_date, "day"), function(date) {
   cat("Fetching EL data from", as.character(date), "\n")
   data <- wmf::build_query("SELECT LEFT(timestamp, 8) AS date,
-                            timestamp AS ts,
-                            userAgent AS user_agent,
-                            event_mwSessionId AS session_id,
-                            event_pageViewId AS page_id,
-                            event_action AS action,
-                            event_checkin AS checkin,
-                            event_position AS result_position,
-                            event_hitsReturned AS results_returned",
+                           timestamp AS ts,
+                           userAgent AS user_agent,
+                           event_mwSessionId AS session_id,
+                           event_pageViewId AS page_id,
+                           event_action AS action,
+                           event_checkin AS checkin,
+                           event_position AS result_position,
+                           event_hitsReturned AS results_returned",
                            date = date,
                            table = "TestSearchSatisfaction2_15357244",
                            conditionals = "event_action IN('searchResultPage', 'visitPage', 'click') AND event_subTest IS NULL")
@@ -36,74 +34,37 @@ events$test_group[is.na(events$test_group)] <- "baseline"
 cat("done.\n")
 
 cat("Writing data (", nrow(events), " events) to disk...", sep = "")
-readr::write_rds(events, "~/satisfaction_click_events.rds", "gz")
+readr::write_rds(events, "~/satisfaction_click_events_new.rds", "gz")
 cat("done.\n")
 
 q(ask = "no")
 
 ## Locally:
 dir.create("data")
-system("scp stat2:/home/bearloga/satisfaction_click_events.rds data/")
+system("scp stat2:/home/bearloga/satisfaction_click_events_new.rds data/")
 
-## Dashboard Data
-# dash_data_desktop <- polloi::read_dataset("search/desktop_event_counts.tsv",
-#                                           col_types = "Tci", skip = 1,
-#                                           col_names = c("date", "action", "events"))
-# dash_data_desktop$platform <- "Desktop"
-dash_data_apps <- polloi::read_dataset("search/app_event_counts",
-                                       col_types = "Tcci", skip = 1,
-                                       col_names = c("date", "action", "platform", "events"))
-dash_data_mobile <- polloi::read_dataset("search/mobile_event_counts",
-                                         col_types = "Tci", skip = 1,
-                                         col_names = c("date", "action", "events"))
-dash_data_mobile$platform <- "Mobile Web"
-dash_data <- dplyr::bind_rows(dash_data_apps, dash_data_mobile)
-dash_data %<>% keep_where(action %in% c("Result pages opened", "clickthroughs"))
-# dash_data <- dash_data[dash_data$date > "2016-04-01" & dash_data$date <= "2016-04-24", ]
-dash_data <- dash_data[dash_data$date > "2016-03-25" & dash_data$date <= "2016-04-24", ]
-readr::write_rds(dash_data, "data/dash_data.rds")
-
-events <- data.table::as.data.table(readr::read_rds("data/satisfaction_click_events.rds"))
+events_new <- data.table::as.data.table(readr::read_rds("data/satisfaction_click_events.rds"))
 # events <- events[events$date > "2016-04-01" & events$date <= "2016-04-24",,]
 # Let's make sure we're only using sessions that had at least one SERP
 #   (sessions with clicks but not SERPs are invalid)
-valid_sessions <- unique(events$session_id[events$action == "searchResultPage"])
-events <- events[events$session_id %in% valid_sessions,,]
+valid_sessions <- unique(events_new$session_id[events_new$action == "searchResultPage"])
+events_new <- events_new[events_new$session_id %in% valid_sessions,,]
 # Weird: SERPs appear multiple times! (with same num of results returned)
 # Need to do some more cleaning
-dupes <- duplicated(events, by = c("session_id", "action", "page_id", "result_position", "results_returned"))
-events <- events[!dupes,,]
+dupes <- duplicated(events_new, by = c("session_id", "action", "page_id", "result_position", "results_returned"))
+events_new <- events_new[!dupes,,]
 # Let's convert to a more usable format:
-events %<>% as.data.frame %>% dplyr::tbl_df()
+events_new %<>% as.data.frame %>% dplyr::tbl_df()
 rm(dupes, valid_sessions)
 
-nonzero_session_ids <- events %>%
+nonzero_session_ids_new <- events_new %>%
   group_by(session_id) %>%
   summarize(any_results = any(results_returned > 0)) %>%
   keep_where(any_results) %>%
   { .$session_id }
 
-ctr_overall <- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
-  group_by(session_id) %>%
-  summarize(visited_page = any(action %in% "visitPage"),
-            clicked = any(action %in% "click" & !is.na(result_position))) %>%
-  summarize(`clickthrough rate (via visitPage, session-wise)` = mean(visited_page),
-            `clickthrough rate (via click, session-wise)` = mean(clicked)) %>%
-  tidyr::gather(type, ctr) %>%
-  mutate(platform = "Desktop")
-# Use the page ids that SERPs and click events share
-ctr_overall_extra <- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
-  group_by(session_id, page_id) %>%
-  summarize(clickthrough = all(c("searchResultPage", "click") %in% action)) %>%
-  ungroup %>%
-  summarize(`clickthrough rate (via click, SERP-wise)` = mean(clickthrough)) %>%
-  tidyr::gather(type, ctr) %>%
-  mutate(platform = "Desktop")
-readr::write_rds(dplyr::bind_rows(ctr_overall, ctr_overall_extra), "data/ctr_overall.rds")
-ctr_daily <- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
+ctr_daily_new <- events_new %>%
+  keep_where(session_id %in% nonzero_session_ids_new) %>%
   group_by(date, session_id) %>%
   summarize(visited_page = any(action %in% "visitPage"),
             clicked = any(action %in% "click" & !is.na(result_position))) %>%
@@ -113,19 +74,18 @@ ctr_daily <- events %>%
   tidyr::gather(type, ctr, -date) %>%
   mutate(platform = "Desktop")
 # Use the page ids that SERPs and click events share
-ctr_daily_extra <- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
+ctr_daily_extra_new <- events_new %>%
+  keep_where(session_id %in% nonzero_session_ids_new) %>%
   group_by(date, session_id, page_id) %>%
   summarize(clickthrough = all(c("searchResultPage", "click") %in% action)) %>%
   group_by(date) %>%
   summarize(`clickthrough rate (via click, SERP-wise)` = mean(clickthrough)) %>%
   tidyr::gather(type, ctr, -date) %>%
   mutate(platform = "Desktop")
-readr::write_rds(dplyr::bind_rows(ctr_daily, ctr_daily_extra), "data/ctr_daily.rds")
 
-click_visits <- events %>%
+click_visits_new <- events_new %>%
   # head(1000) %>% # for prototyping
-  keep_where(session_id %in% nonzero_session_ids, action != "searchResultPage") %>%
+  keep_where(session_id %in% nonzero_session_ids_new, action != "searchResultPage") %>%
   slice_rows(c("date", "session_id")) %>%
   by_slice(function(slice) {
     n_clicks <- sum(slice$action == "click" & !is.na(slice$result_position))
@@ -161,4 +121,3 @@ click_visits <- events %>%
   }, .collate = "rows") %>%
   mutate(`clicks not accounted for` = valid_clicks - matches,
          `visits not accounted for` = valid_visits - matches)
-readr::write_rds(click_visits, "data/click_visits.rds")
--- eda.R
+++ eda.R
@@ -7,78 +7,26 @@ library(purrr)
 
 dir.create("figures")
 
-events <- data.table::as.data.table(readr::read_rds("data/satisfaction_click_events.rds"))
-events <- events[events$date > "2016-04-01" & events$date <= "2016-04-24",,]
-# Let's make sure we're only using sessions that had at least one SERP
-#   (sessions with clicks but not SERPs are invalid)
-valid_sessions <- unique(events$session_id[events$action == "searchResultPage"])
-events <- events[events$session_id %in% valid_sessions,,]
-# Weird: SERPs appear multiple times! (with same num of results returned)
-# Need to do some more cleaning
-dupes <- duplicated(events, by = c("session_id", "action", "page_id", "result_position", "results_returned"))
-events <- events[!dupes,,]
-# Let's convert to a more usable format:
-events %<>% as.data.frame %>% dplyr::tbl_df()
-rm(dupes, valid_sessions)
-
-nonzero_session_ids <- events %>%
-  group_by(session_id) %>%
-  summarize(any_results = any(results_returned > 0)) %>%
-  keep_where(any_results) %>%
-  { .$session_id }
-
-# Use the page ids that SERPs and click events share
-
-ctr_overall <- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
-  group_by(session_id) %>%
-  summarize(visited_page = any(action %in% "visitPage"),
-            clicked = any(action %in% "click" & !is.na(result_position))) %>%
-  summarize(`clickthrough rate (via visitPage, session-wise)` = mean(visited_page),
-            `clickthrough rate (via click, session-wise)` = mean(clicked)) %>%
-  tidyr::gather(type, ctr) %>%
-  mutate(platform = "Desktop")
-ctr_overall_extra <- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
-  group_by(session_id, page_id) %>%
-  summarize(clickthrough = all(c("searchResultPage", "click") %in% action)) %>%
-  ungroup %>%
-  summarize(`clickthrough rate (via click, SERP-wise)` = mean(clickthrough)) %>%
-  tidyr::gather(type, ctr) %>%
-  mutate(platform = "Desktop")
-ctr_daily<- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
-  group_by(date, session_id) %>%
-  summarize(visited_page = any(action %in% "visitPage"),
-            clicked = any(action %in% "click" & !is.na(result_position))) %>%
-  group_by(date) %>%
-  summarize(`clickthrough rate (via visitPage, session-wise)` = mean(visited_page),
-            `clickthrough rate (via click, session-wise)` = mean(clicked)) %>%
-  tidyr::gather(type, ctr, -date) %>%
-  mutate(platform = "Desktop")
-ctr_daily_extra <- events %>%
-  keep_where(session_id %in% nonzero_session_ids) %>%
-  group_by(date, session_id, page_id) %>%
-  summarize(clickthrough = all(c("searchResultPage", "click") %in% action)) %>%
-  group_by(date) %>%
-  summarize(`clickthrough rate (via click, SERP-wise)` = mean(clickthrough)) %>%
-  tidyr::gather(type, ctr, -date) %>%
-  mutate(platform = "Desktop")
-
 dash_data <- readr::read_rds("data/dash_data.rds")
 dash_overall <- dash_data %>%
   tidyr::spread(action, events) %>%
   group_by(platform) %>%
   summarize(clickthroughs = sum(clickthroughs),
             SERPs = sum(`Result pages opened`)) %>%
-  mutate(`clickthrough rate (via Schema:Search)` = clickthroughs/SERPs) %>%
-  select(c(platform, `clickthrough rate (via Schema:Search)`)) %>%
-  tidyr::gather(type, ctr, -platform)
+  mutate(`clickthrough rate` = clickthroughs/SERPs) %>%
+  select(c(platform, `clickthrough rate`)) %>%
+  tidyr::gather(type, ctr, -platform) %>%
+  mutate(type = ifelse(platform == "Desktop", paste(type, "(via Schema:Search)"), type)) %>%
+  mutate(type = ifelse(platform == "Mobile Web", paste(type, "(via Schema:MobileWebSearch)"), type)) %>%
+  mutate(type = ifelse(platform %in% c("Android", "iOS"), paste(type, "(via Schema:MobileWikiAppSearch)"), type))
 dash_daily <- dash_data %>%
   tidyr::spread(action, events) %>%
-  mutate(`clickthrough rate (via Schema:Search)` = clickthroughs/`Result pages opened`) %>%
-  select(c(date, platform, `clickthrough rate (via Schema:Search)`)) %>%
-  tidyr::gather(type, ctr, -c(date, platform))
+  mutate(`clickthrough rate` = clickthroughs/`Result pages opened`) %>%
+  select(c(date, platform, `clickthrough rate`)) %>%
+  tidyr::gather(type, ctr, -c(date, platform)) %>%
+  mutate(type = ifelse(platform == "Desktop", paste(type, "(via Schema:Search)"), type)) %>%
+  mutate(type = ifelse(platform == "Mobile Web", paste(type, "(via Schema:MobileWebSearch)"), type)) %>%
+  mutate(type = ifelse(platform %in% c("Android", "iOS"), paste(type, "(via Schema:MobileWikiAppSearch)"), type))
 
 p <- ggplot(data = dplyr::bind_rows(ctr_daily, ctr_daily_extra,
                                     keep_where(dash_daily, platform != "Desktop")),
@@ -93,9 +41,10 @@ p <- ggplot(data = dplyr::bind_rows(ctr_daily, ctr_daily_extra,
   ggthemes::theme_tufte(base_family = "Gill Sans", base_size = 12) +
   theme(legend.position = "bottom", panel.grid = element_line(color = "black", size = 0.1)) +
   scale_color_manual(values = RColorBrewer::brewer.pal(7, "Set1")[-6],
-                     guide = guide_legend(title = "Clickthrough & Platform", nrow = 3))
+                     guide = guide_legend(title = "Method & Platform", nrow = 3))
 print(p)
 ggsave("daily_ctr.png", p, path = "figures", width = 10, height = 6, dpi = 150)
+# ggsave("daily_ctr_mini.png", p, path = "figures", width = 10, height = 6, dpi = 100)
 
 png("figures/ctr_matrix.png", width = 18, height = 10, res = 150, units = "in", pointsize = 14)
 ctr_daily %>%
@@ -123,4 +72,155 @@ click_visits %>%
             `sessions with valid visits that couldn't be matched with valid clicks` = sum(valid_clicks > 0 & valid_visits > 0 & `visits not accounted for` > 0)/n()) %>%
   tidyr::gather(" ", `proportion of sessions`) %>%
   mutate(`proportion of sessions` = sprintf("%.3f%%", 100*`proportion of sessions`)) %>%
+  knitr::kable(align = c("l", "r"), format = "markdown") %>%
   knitr::kable(align = c("l", "r"), format = "latex", caption = "...")
+
+p <- click_visits %>%
+  keep_where((valid_clicks == 0 & valid_visits == 0) | (`clicks not accounted for` == 0 & `visits not accounted for` == 0)) %>%
+  group_by(date) %>%
+  summarize(`total sessions` = n(),
+            `abandoned sessions` = sum(valid_clicks == 0),
+            `clickthrough'd sessions` = sum(matches > 0),
+            `clickthrough rate (via click-visit matching)` = mean(matches > 0)) %>%
+  select(c(date, `clickthrough rate (via click-visit matching)`)) %>%
+  tidyr::gather(type, ctr, -date) %>%
+  mutate(platform = "Desktop") %>%
+  dplyr::bind_rows(ctr_daily, ctr_daily_extra) %>%
+  ggplot(aes(x = date, y = ctr, color = paste(type, "on", platform))) +
+  geom_line(size = 1.5) + geom_point(size = 2) +
+  scale_x_datetime(date_breaks = "1 week", date_labels = "%a (%d %b)") +
+  scale_y_continuous("Clickthrough rate", labels = scales::percent_format()) +
+  labs(title = "Proportions of sessions or searches where user clicked on a result") +
+  ggthemes::theme_tufte(base_family = "Gill Sans", base_size = 12) +
+  theme(legend.position = "bottom", panel.grid = element_line(color = "black", size = 0.1)) +
+  scale_color_manual(values = RColorBrewer::brewer.pal(4, "Set1")[c(1, 3, 2, 4)],
+                     guide = guide_legend(title = "Method & Platform", nrow = 2))
+print(p)
+ggsave("daily_ctr_2.png", p, path = "figures", width = 10, height = 6, dpi = 150)
+ggsave("daily_ctr_2_mini.png", p, path = "figures", width = 10, height = 6, dpi = 100)
+
+ctrs <- click_visits %>%
+  keep_where((valid_clicks == 0 & valid_visits == 0) | (`clicks not accounted for` == 0 & `visits not accounted for` == 0)) %>%
+  group_by(date) %>%
+  summarize(`total sessions` = n(),
+            `abandoned sessions` = sum(valid_clicks == 0),
+            `clickthrough'd sessions` = sum(matches > 0),
+            `clickthrough rate (via click-visit matching)` = mean(matches > 0)) %>%
+  select(c(date, `clickthrough rate (via click-visit matching)`)) %>%
+  tidyr::gather(type, ctr, -date) %>%
+  mutate(platform = "Desktop") %>%
+  dplyr::bind_rows(ctr_daily, ctr_daily_extra) %>%
+  mutate(ctr = 100*ctr, type = paste(type, "on", platform)) %>%
+  select(-platform) %>%
+  tidyr::spread(type, ctr) %>%
+  select(-date)
+
+png("figures/ctr_matrix_2.png", width = 18, height = 10, res = 150, units = "in", pointsize = 14)
+pairs(ctrs, pch = 16)
+dev.off()
+
+# A stats test of Granger causality (whether x can be used to forecast y)
+plot(ctrs$`clickthrough rate (via click, session-wise) on Desktop`, type = "l", lwd = 2)
+lines(ctrs$`clickthrough rate (via click-visit matching) on Desktop`, col = "red", lwd = 2)
+lmtest::grangertest(ctrs$`clickthrough rate (via click, session-wise) on Desktop`[-nrow(ctrs)],
+                    ctrs$`clickthrough rate (via click-visit matching) on Desktop`[-1], order = 1)
+# p-val 0.2488
+lmtest::grangertest(ctrs$`clickthrough rate (via click, session-wise) on Desktop`,
+                    ctrs$`clickthrough rate (via click-visit matching) on Desktop`, order = 1)
+# p-val 0.4876
+
+library(forecast)
+fit <- auto.arima(x = ctrs$`clickthrough rate (via click-visit matching) on Desktop`,
+                  xreg = as.matrix(ctrs[, c(2, 3)]), seasonal = TRUE, stationary = TRUE,
+                  stepwise = FALSE, start.P = 1)
+plot(ctrs$`clickthrough rate (via click-visit matching) on Desktop`, type = "l", lwd = 2)
+lines(as.numeric(fitted(fit)), col = "red")
+
+# > broom::tidy(fit)
+# Model 1:
+#                                                     term   estimate std.error
+# 1                                              intercept 12.1001089 6.3221226
+# 2    clickthrough rate (via click, SERP-wise) on Desktop -0.7665743 0.1909358
+# 3 clickthrough rate (via click, session-wise) on Desktop  1.3211061 0.2353843
+# Model 2:
+#                                                     term   estimate std.error
+# 1    clickthrough rate (via click, SERP-wise) on Desktop -0.7735988 0.1529176
+# 2 clickthrough rate (via click, session-wise) on Desktop  1.5576939 0.1125968
+
+# Version 1
+ctrs$`clickthrough rate (via model 1) on Desktop` <- 12.1001 +
+  -0.7666 * ctrs$`clickthrough rate (via click, SERP-wise) on Desktop` +
+  1.3211 * ctrs$`clickthrough rate (via click, session-wise) on Desktop`
+# Version 2
+ctrs$`clickthrough rate (via model 2) on Desktop` <- -0.7735988 * ctrs$`clickthrough rate (via click, SERP-wise) on Desktop` +
+  1.5576939 * ctrs$`clickthrough rate (via click, session-wise) on Desktop`
+# Version 3
+ctrs$`clickthrough rate (via avg model) on Desktop` <- (ctrs$`clickthrough rate (via model 1) on Desktop` + ctrs$`clickthrough rate (via model 2) on Desktop`)/2
+# Plot it!
+plot(ctrs$`clickthrough rate (via click-visit matching) on Desktop`, type = "l", lwd = 2)
+lines(ctrs$`clickthrough rate (via model 1) on Desktop`, col = "red", lwd = 2)
+lines(ctrs$`clickthrough rate (via model 2) on Desktop`, col = "blue", lwd = 2)
+lines(ctrs$`clickthrough rate (via avg model) on Desktop`, col = "green", lwd = 2)
+
+mae <- function(actual, fitted) { # mean absolute error
+  return(sum(abs(actual-fitted))/length(actual))
+}
+mae(ctrs$`clickthrough rate (via click-visit matching) on Desktop`, ctrs$`clickthrough rate (via model 1) on Desktop`)
+# = 0.69578
+mae(ctrs$`clickthrough rate (via click-visit matching) on Desktop`, ctrs$`clickthrough rate (via model 2) on Desktop`)
+# = 0.6916616
+mae(ctrs$`clickthrough rate (via click-visit matching) on Desktop`, ctrs$`clickthrough rate (via avg model) on Desktop`)
+# = 0.6799207
+
+ctrs_new <- click_visits_new %>%
+  keep_where((valid_clicks == 0 & valid_visits == 0) | (`clicks not accounted for` == 0 & `visits not accounted for` == 0)) %>%
+  group_by(date) %>%
+  summarize(`total sessions` = n(),
+            `abandoned sessions` = sum(valid_clicks == 0),
+            `clickthrough'd sessions` = sum(matches > 0),
+            `clickthrough rate (via click-visit matching)` = mean(matches > 0)) %>%
+  select(c(date, `clickthrough rate (via click-visit matching)`)) %>%
+  tidyr::gather(type, ctr, -date) %>%
+  mutate(platform = "Desktop") %>%
+  dplyr::bind_rows(ctr_daily_new, ctr_daily_extra_new) %>%
+  mutate(ctr = 100*ctr, type = paste(type, "on", platform)) %>%
+  select(-platform) %>%
+  tidyr::spread(type, ctr) %>%
+  select(-date)
+
+# Version 1
+ctrs_new$`clickthrough rate (via model 1) on Desktop` <- 12.1001 +
+  -0.7666 * ctrs_new$`clickthrough rate (via click, SERP-wise) on Desktop` +
+  1.3211 * ctrs_new$`clickthrough rate (via click, session-wise) on Desktop`
+# Version 2
+ctrs_new$`clickthrough rate (via model 2) on Desktop` <- -0.7735988 * ctrs_new$`clickthrough rate (via click, SERP-wise) on Desktop` +
+  1.5576939 * ctrs_new$`clickthrough rate (via click, session-wise) on Desktop`
+# Version 3
+ctrs_new$`clickthrough rate (via avg model) on Desktop` <- (ctrs_new$`clickthrough rate (via model 1) on Desktop` + ctrs_new$`clickthrough rate (via model 2) on Desktop`)/2
+# Visualize!
+plot(ctrs_new$`clickthrough rate (via click-visit matching) on Desktop`, type = "l", lwd = 1, col = "gray30", xlim = c(1, nrow(ctrs_new)))
+abline(v = 1:nrow(ctrs_new), col = "gray90")
+lines(head(ctrs_new$`clickthrough rate (via click-visit matching) on Desktop`, nrow(ctrs_new) - 3), col = "black", lwd = 2, lty = "solid")
+lines(head(ctrs_new$`clickthrough rate (via model 1) on Desktop`, nrow(ctrs_new) - 3), col = "red", lwd = 2, lty = "solid")
+lines(head(ctrs_new$`clickthrough rate (via model 2) on Desktop`, nrow(ctrs_new) - 3), col = "blue", lwd = 2, lty = "solid")
+lines(head(ctrs_new$`clickthrough rate (via avg model) on Desktop`, nrow(ctrs_new) - 3), col = "green", lwd = 2, lty = "solid")
+lines(nrow(ctrs_new) - c(2:0), tail(ctrs_new$`clickthrough rate (via click-visit matching) on Desktop`, 3), type = "l", lwd = 2, lty = "dashed")
+lines(nrow(ctrs_new) - c(2:0), tail(ctrs_new$`clickthrough rate (via model 1) on Desktop`, 3), col = "red", lwd = 2, lty = "dashed")
+lines(nrow(ctrs_new) - c(2:0), tail(ctrs_new$`clickthrough rate (via model 2) on Desktop`, 3), col = "blue", lwd = 2, lty = "dashed")
+lines(nrow(ctrs_new) - c(2:0), tail(ctrs_new$`clickthrough rate (via avg model) on Desktop`, 3), col = "green", lwd = 2, lty = "dashed")
+
+mae(tail(ctrs_new$`clickthrough rate (via click-visit matching) on Desktop`, 3),
+    tail(ctrs_new$`clickthrough rate (via model 1) on Desktop`, 3))
+# = 0.754515
+mae(tail(ctrs_new$`clickthrough rate (via click-visit matching) on Desktop`, 3),
+    tail(ctrs_new$`clickthrough rate (via model 2) on Desktop`, 3))
+# = 0.5991338
+mae(tail(ctrs_new$`clickthrough rate (via click-visit matching) on Desktop`, 3),
+    tail(ctrs_new$`clickthrough rate (via avg model) on Desktop`, 3))
+# = 0.6640269
+
+# Conclusion: we can use a very well trained model to estimate "clickthrough rate via click-visit matching"
+#             in the absense of visitPage events, using only click events as a proxy, or more specifically:
+#             the session-wise and SERP-wise clickthrough rates. To compare test vs controls, we compare
+#             the estimated clickthrough, preferably in a probabilistic, Bayesian framework because we're
+#             comparing indirectly observed (well, unobserved) random variable quantities.
